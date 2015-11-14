8da1a8bc3e6c70dd200b5f82924f8ccb0bc65b24
Sam Hager <sam.hager11@gmail.com>
2015-11-13 17:23:36 -0800

    merge conflict for variable and reuqirement declaration

--- app.js
+++ app.js
@@ -1,10 +1,21 @@
-var express = require('express'),
-    app = express(),
-    port = process.env.PORT || 3000,
-	mongoose	= require('mongoose');
+var express     = require('express'),
+    app         = express(),
+    port        = process.env.PORT || 3000,
+    logger      = require('morgan'),
+    bodyParser  = require('body-parser'),
+	     mongoose	= require('mongoose'),
 
 //establish connection to mongo database
 mongoose.connect('mongodb://localhost/koreanBBQdb')
+
+
+//track requests made to the app
+app.use(logger('dev'))
+
+//makes json object available in requests
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({extended : false}))
+
 
 app.listen(port, function() {
   console.log('Application server started successfully on port: ' + port)
